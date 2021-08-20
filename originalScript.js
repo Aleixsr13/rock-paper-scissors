987ae6b95f22f09addfc7f698d58805ea4d8be44
aleix <aleixsr13@gmail.com>
2021-08-21 01:52:39 +0200

    rock-paper-scissors with 'UI'

---
+++ originalScript.js
@@ -0,0 +1,84 @@
+function computerPlay(){
+    let move = ["Rock", "Paper", "Scissors"];
+    return move[Math.floor(Math.random()*move.length)];
+}
+
+let computerScore = 0;
+let playerScore = 0;
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === "rock"){
+        if(computerSelection === "Scissors"){
+            playerScore ++;
+            return `You Win! Rock beats Scissors.
+Score: Player ${playerScore} - ${computerScore} Computer`;
+        }
+        else if (computerSelection === "Paper"){
+            computerScore++;
+            return `You Lose! Paper beats Rock.
+Score: Player ${playerScore} - ${computerScore} Computer`;
+        }
+        else {
+            return `It's a draw.
+Score: Player ${playerScore} - ${computerScore} Computer`;
+        }
+    }
+
+    if(playerSelection === "paper"){
+        if(computerSelection === "Rock"){
+            playerScore++;
+            return `You Win! Paper beats Rock.
+Score: Player ${playerScore} - ${computerScore} Computer`;
+        }
+        else if (computerSelection === "Scissors"){
+            computerScore++;
+            return `You Lose! Scissors beats Paper.
+Score: Player ${playerScore} - ${computerScore} Computer`;
+        }
+        else {
+            return `It's a draw.
+Score: Player ${playerScore} - ${computerScore} Computer`;
+        }
+    }
+
+    if(playerSelection === "scissors"){
+        if(computerSelection === "Paper"){
+            playerScore++;
+            return `You Win! Scissors beats Paper.
+Score: Player ${playerScore} - ${computerScore} Computer`;
+        }
+        else if (computerSelection === "Rock"){
+            computerScore++;
+            return `You Lose! Rock beats Scissors.
+Score: Player ${playerScore} - ${computerScore} Computer`;
+        }
+        else {
+            return `It's a draw.
+Score: Player ${playerScore} - ${computerScore} Computer`;
+        }
+    }
+}
+
+function game(){
+    /*for (let i = 0; i < 5; i++){
+        let playerSelection = prompt("Type Rock, Paper or Scissors");
+        playerSelection = playerSelection.toLowerCase();
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }*/
+
+    if(playerScore > computerScore){
+        console.log(`You have won! 
+Final score is: ${playerScore} - ${computerScore}`)
+    }
+    else if(playerScore < computerScore){
+        console.log(`You lost! 
+Final score is: ${playerScore} - ${computerScore}`)
+    }
+    else{
+        console.log(`You tied.
+Final score is: ${playerScore} - ${computerScore}`)
+    }
+}
+
+game();
--- script.js
+++ script.js
@@ -1,84 +1,50 @@
-function computerPlay(){
-    let move = ["Rock", "Paper", "Scissors"];
-    return move[Math.floor(Math.random()*move.length)];
-}
-
 let computerScore = 0;
 let playerScore = 0;
+const buttons = document.querySelectorAll("input");
 
-function playRound(playerSelection, computerSelection){
-    if(playerSelection === "rock"){
-        if(computerSelection === "Scissors"){
-            playerScore ++;
-            return `You Win! Rock beats Scissors.
-Score: Player ${playerScore} - ${computerScore} Computer`;
-        }
-        else if (computerSelection === "Paper"){
-            computerScore++;
-            return `You Lose! Paper beats Rock.
-Score: Player ${playerScore} - ${computerScore} Computer`;
-        }
-        else {
-            return `It's a draw.
-Score: Player ${playerScore} - ${computerScore} Computer`;
-        }
-    }
-
-    if(playerSelection === "paper"){
-        if(computerSelection === "Rock"){
-            playerScore++;
-            return `You Win! Paper beats Rock.
-Score: Player ${playerScore} - ${computerScore} Computer`;
-        }
-        else if (computerSelection === "Scissors"){
-            computerScore++;
-            return `You Lose! Scissors beats Paper.
-Score: Player ${playerScore} - ${computerScore} Computer`;
-        }
-        else {
-            return `It's a draw.
-Score: Player ${playerScore} - ${computerScore} Computer`;
-        }
-    }
-
-    if(playerSelection === "scissors"){
-        if(computerSelection === "Paper"){
-            playerScore++;
-            return `You Win! Scissors beats Paper.
-Score: Player ${playerScore} - ${computerScore} Computer`;
-        }
-        else if (computerSelection === "Rock"){
-            computerScore++;
-            return `You Lose! Rock beats Scissors.
-Score: Player ${playerScore} - ${computerScore} Computer`;
-        }
-        else {
-            return `It's a draw.
-Score: Player ${playerScore} - ${computerScore} Computer`;
-        }
-    }
+function computerPlay() {
+  let moves = ["Rock", "Paper", "Scissors"];
+  return moves[Math.floor(Math.random() * moves.length)];
 }
 
-function game(){
-    for (let i = 0; i < 5; i++){
-        let playerSelection = prompt("Type Rock, Paper or Scissors");
-        playerSelection = playerSelection.toLowerCase();
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-    }
+function playRound(playerSelection) {
+  let computerSelection = computerPlay();
+  let result = "";
 
-    if(playerScore > computerScore){
-        console.log(`You have won! 
-Final score is: ${playerScore} - ${computerScore}`)
+  if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")
+  ) {
+    playerScore++;
+    result = `You Win! ${playerSelection} beats ${computerSelection} <br><br>Score: Player ${playerScore} - ${computerScore} Computer`;
+    if (playerScore === 5) {
+      result += "<br><br><h2>You have won!</h2>";
+      disableButtons();
     }
-    else if(playerScore < computerScore){
-        console.log(`You lost! 
-Final score is: ${playerScore} - ${computerScore}`)
+  } else if (playerSelection === computerSelection) {
+    result = `It's a draw. You both chose ${playerSelection} <br><br>Score: Player ${playerScore} - ${computerScore} Computer`;
+  } else {
+    computerScore++;
+    result = `You Lose! ${computerSelection} beats ${playerSelection} <br><br>Score: Player ${playerScore} - ${computerScore} Computer`;
+    if (computerScore === 5) {
+      result += "<br><br><h2>You lost!</h2>";
+      disableButtons();
     }
-    else{
-        console.log(`You tied.
-Final score is: ${playerScore} - ${computerScore}`)
-    }
+  }
+
+  document.getElementById("results").innerHTML = result;
+  return;
 }
 
-game();
+function disableButtons() {
+  buttons.forEach((e) => {
+    e.disabled = true;
+  });
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    playRound(button.value);
+  });
+});
